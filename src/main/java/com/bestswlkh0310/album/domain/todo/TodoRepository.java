25a411882604765bcdf6b80c96703fc890a3f0a0
bswlkh0310 <hhhello0507@dgsw.hs.kr>
2023-07-24 19:00:34 +0900

    d

--- src/main/java/com/bestswlkh0310/album/domain/todo/TodoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.bestswlkh0310.album.domain.todo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
